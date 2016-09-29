45adf951c815929cd33294a3bbbef1f73ffd026a
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-09-29 16:17:11 -0600

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ function getHomePage(req, res, next) {
         title: 'Office Anywhere',
         brand: 'Office Anywhere',
         verified: req.isAuthenticated(),
+        user: req.user
     })
 }
 router.get('/login', function(req, res, next) {
